dec322e12188921a949ee9e835b507747816a033
GaGandour <gagandour@gmail.com>
2022-09-10 19:46:16 -0300

    token length

--- app/logic/user.py
+++ app/logic/user.py
@@ -7,4 +7,4 @@ def hash_password(password):
 
 def generate_token():
     alphabet = string.ascii_letters + string.digits
-    return ''.join(secrets.choice(alphabet) for i in range(8))
+    return ''.join(secrets.choice(alphabet) for i in range(20))
