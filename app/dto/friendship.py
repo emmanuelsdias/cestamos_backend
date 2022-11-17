9ce137ce1cb2d79d955b076ee91a3143983262fb
GaGandour <gagandour@gmail.com>
2022-11-17 08:57:48 -0300

    improved get friendship endpoint

--- app/dto/friendship.py
+++ app/dto/friendship.py
@@ -6,7 +6,5 @@ from app.dto.output_dto import OutputBaseModel
 class Friendship(OutputBaseModel):
     friendship_id: int
     
-    user_id1: int
-    username1: str
-    user_id2: int
-    username2: str
+    user_id: int
+    username: str
