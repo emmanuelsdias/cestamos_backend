094849672d9dac2a8f3d4538410a2d189555c936
Gabriel Gandour <gagandour@gmail.com>
2023-04-14 19:57:25 -0300

    git database

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from dal.database import Base
 
 
@@ -8,6 +8,22 @@ class Recipe(Base):
     recipe_id = Column(Integer, primary_key=True, index=True)
 
     user_id = Column(Integer, ForeignKey("users.user_id"))
+
     name = Column(String(50))
+    description = Column(String)
     ingredients = Column(String)
+    people_served = Column(Integer)
     instructions = Column(String)
+    prep_time = Column(String(100))
+    cooking_time = Column(String(100))
+    resting_time = Column(String(100))
+    is_public = Column(Boolean, default=False)
+
+
+class RecipeList(Base):
+    __tablename__ = "recipe_lists"
+
+    recipe_list_id = Column(Integer, primary_key=True, index=True)
+
+    recipe_id = Column(Integer, ForeignKey("recipes.recipe_id"))
+    shop_list_id = Column(Integer, ForeignKey("shop_lists.shop_list_id"))
