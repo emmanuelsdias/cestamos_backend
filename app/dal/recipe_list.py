22b7d869dff918c135e41e238a1461dd0e6f8c50
William Ryu Gyotoku <wrgyotoku@gmail.com>
2023-04-15 17:12:41 -0300

    add recipe list

---
+++ app/dal/recipe_list.py
@@ -0,0 +1,28 @@
+import abc
+from sqlalchemy.orm import Session
+from sqlalchemy import and_
+
+from models.recipe import RecipeList
+
+from typing import List
+
+
+class ABCRecipeListDal:
+    @abc.abstractmethod
+    def create_recipe_list(self, recipe_list: RecipeList) -> RecipeList:
+        """Adds recipe to list"""
+
+
+class RecipeListDal(ABCRecipeListDal):
+    def __init__(self, db_session: Session):
+        self.db: Session = db_session
+
+    def create_recipe_list(self, recipe_list: RecipeList) -> RecipeList:
+        self.db.add(recipe_list)
+        self.db.commit()
+        self.db.refresh(recipe_list)
+        return recipe_list
+
+    def get_recipe_list_by_shop_list_id_and_recipe_id(self,shop_list_id: int, recipe_id: int) -> RecipeList:
+        return self.db.query(RecipeList).filter(and_(RecipeList.shop_list_id == shop_list_id, RecipeList.recipe_id == recipe_id)).first()
+    
--- app/factories/shop_list.py
+++ app/factories/shop_list.py
@@ -5,6 +5,8 @@ from dal.user import ABCUserDal
 from dal.friendship import ABCFriendshipDal
 from dal.item import ABCItemDal
 from dal.user_list import ABCUserListDal
+from dal.recipe_list import ABCRecipeListDal
+from dal.recipe import ABCRecipeDal
 from factories.dal_factories import (
     get_user_dal,
     get_shop_list_dal,
@@ -12,6 +14,8 @@ from factories.dal_factories import (
     get_item_dal,
     get_user_list_dal,
     get_user_list_dal,
+    get_recipe_list_dal,
+    get_recipe_dal,
 )
 from services.shop_list import ABCShopListService, ShopListService
 
@@ -22,9 +26,11 @@ def get_shop_list_service(
     item_dal: ABCItemDal = Depends(get_item_dal),
     user_dal: ABCUserDal = Depends(get_user_dal),
     user_list_dal: ABCUserListDal = Depends(get_user_list_dal),
+    recipe_list_dal: ABCRecipeListDal = Depends(get_recipe_list_dal),
+    recipe_dal: ABCRecipeDal = Depends(get_recipe_dal),
 ) -> ABCShopListService:
     client = ShopListService(
-        shop_list_dal, friendship_dal, item_dal, user_dal, user_list_dal
+        shop_list_dal, friendship_dal, item_dal, user_dal, user_list_dal, recipe_list_dal, recipe_dal
     )
     try:
         yield client
